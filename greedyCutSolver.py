b32807260226d6506da0fcb3c4b148126d2d9049
Taj Shaik <tajshaik24@gmail.com>
2018-11-28 15:07:08 -0800

    Added in computing negative edges

--- greedyCutSolver.py
+++ greedyCutSolver.py
@@ -3,7 +3,7 @@ import os
 import matplotlib.pyplot as plt
 import random
 import sys
-
+import itertools
 
 def main():
 	inputs = readInput("/all_inputs/small/104")
@@ -13,21 +13,31 @@ def main():
 	constraints = inputs[3]
 	edges_dict = computeRowdyEdges(constraints)
 	newG = addRowdyEdges(G, edges_dict)
+	#Use min-cut algorithm to make the cuts of max size size_buses
+	#Recursively partition until we have it num_buses
+
+def computeRowdyEdges(constraints):
+    edge_weights = {}
+    for x in constraints:
+        length = len(x)
+        all_edges = list(itertools.combinations(x, 2))
+        for edge in all_edges:
+            if(edge in edge_weights):
+                edge_weights[edge] = max(edge_weights.get(edge), -1/length)
+            else:
+                edge_weights[edge] = -1/length
+    return edge_weights
 	
-
-
 def readInput(input_folder):
 	graph = nx.read_gml(input_folder + "/graph.gml")
     parameters = open(input_folder + "/parameters.txt")
     num_buses = int(parameters.readline())
     size_bus = int(parameters.readline())
     constraints = []
-
     for line in parameters:
         line = line[1: -2]
         curr_constraint = [node.replace("'","") for node in line.split(", ")]
         constraints.append(curr_constraint)
-
     return (graph, num_buses, size_bus, constraints)
 
 
